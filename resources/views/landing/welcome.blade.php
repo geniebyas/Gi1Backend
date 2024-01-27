8f3aa7c76733b042ef0250f24fe2032aee9b0702
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-28 00:00:50 +0530

    some view level changes

--- resources/views/landing/welcome.blade.php
+++ resources/views/landing/welcome.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Super App</title>
+    <title>Gi1 Super App</title>
 </head>
-<body style="align-items:center;justify-content:center;">
-    <h1>Welcome to Gi1 Super App!</h1>
+<body style="width:100%;align-items:center;justify-content:center;">
+    <h1 style="text-align: center;">Welcome to Gi1 Super App!</h1>
     <p>This is a simple landing page for api.gi1superapp.com</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing/welcome');
 });
 
 require __DIR__.'/auth.php';
