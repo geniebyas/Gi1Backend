29d5e1dc1917cd623427da1bbf1722b318703b31
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-28 00:04:12 +0530

    asdf asdf

--- resources/views/landing/welcome.blade.php
+++ resources/views/landing/welcome.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gi1 Super App</title>
+
 </head>
 <body style="width:100%;align-items:center;justify-content:center;">
     <h1 style="text-align: center;">Welcome to Gi1 Super App!</h1>
-    <p>This is a simple landing page for api.gi1superapp.com</p>
+    <p style="text-align: center;">This is a simple landing page for api.gi1superapp.com</p>
 </body>
 </html>
