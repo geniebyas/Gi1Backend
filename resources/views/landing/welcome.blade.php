3d3d83751c79765576f6f670f43761567b76e8c7
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-28 00:28:39 +0530

    welcome page

--- resources/views/landing/welcome.blade.php
+++ resources/views/landing/welcome.blade.php
@@ -8,6 +8,18 @@
 
 </head>
 <body style="width:100%;align-items:center;justify-content:center;">
+          <div>
+            <img src="images/logoelevated.jpg" alt="Logo" style="max-width: 50%;
+            max-height: 50%;
+            border-radius: 20px;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);">
+          </div>
+        <h3 class="text-center" style="
+                  color: #ffffff;
+                  font-weight: 100px;
+                  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+                  border-radius: 0 0 15px 15px; 
+                  margin: 0;">Gi1 Super App</h3>      
     <h1 style="text-align: center;">Welcome to Gi1 Super App!</h1>
     <p style="text-align: center;">We have an exiting news for you <a href="https://gi1superapp.com">Click me.</a></p>
 </body>
