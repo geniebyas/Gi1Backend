7dbe5d3e77a156dbc47348e8d01db3c3db92ade3
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-27 20:31:48 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('welcome');
 });
 
 require __DIR__.'/auth.php';
