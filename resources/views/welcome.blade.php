b86a82f85c0f61c5020ec2392b7a2702846464af
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-27 20:34:46 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Super App</title>
 </head>
-<body>
-    <h1>Welcome to My Super App!</h1>
-    <p>This is a simple landing page for api.gi1superapp.com.</p>
+<body style="align-items:center;justify-content:center;">
+    <h1>Welcome to Gi1 Super App!</h1>
+    <p>This is a simple landing page for api.gi1superapp.com</p>
 </body>
 </html>
