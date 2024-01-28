4d37bc69e1772437f4c9ebff78b2489cad64fbff
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-28 09:08:26 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '13.53.54.72'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
