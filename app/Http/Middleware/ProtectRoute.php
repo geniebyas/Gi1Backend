a110df3558de76c812f94b3153b07588d4b0461f
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-02-03 13:26:18 +0530

    Update ProtectRoute.php

--- app/Http/Middleware/ProtectRoute.php
+++ app/Http/Middleware/ProtectRoute.php
@@ -18,7 +18,9 @@ class ProtectRoute
     {
         $uid = $request->header('uid');
         $user = User::where('uid', $uid)->get()->first();
+echo $request->path();
       if($request->path() == "register" || $request->path() == "checkuserexists/{uid}"){
+
 return $next($request);
         } else if (is_null($user)) {
             $response = [
