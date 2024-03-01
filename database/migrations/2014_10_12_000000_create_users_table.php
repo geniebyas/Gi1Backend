80200fa4cf7bb295bf271fdfec182beac635b52d
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-02 02:02:50 +0530

    asdfasdf

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('uid')->unique();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->string('phone')->nullable();
