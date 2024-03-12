999129363ccb68ba10862d0b0f763184a02916cb
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-12 10:38:11 +0530

    asd fsdf

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Model
 
 
     public function settings(){
-        return $this->hasOne("App/Models/UsersSetting",'uid');
+        return $this->hasOne(UsersSetting::class,"uid");
     }
 
     public function responses(){
