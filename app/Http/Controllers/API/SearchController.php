5f9c2f97e600e458666bc10b677007645b246239
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-13 14:54:27 +0530

    sadfsdf

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -13,13 +13,14 @@ class SearchController extends Controller
     //
     function globalSearch($query,$filter){
 
-        $users = User::with('wallet')
-        ->where(function (Builder $queryBuilder) use ($query) {
+        $users = User::
+        where(function (Builder $queryBuilder) use ($query) {
             $queryBuilder
                 ->where('name', 'LIKE', "%$query%")
                 ->orWhere('username', 'LIKE', "%$query%")
                 ->orWhere('email', 'LIKE', "%$query%");
         })
+        ->with('wallet')
         ->get();
 
         $industries = Industry::where(function (Builder $queryBuilder) use ($query) {
