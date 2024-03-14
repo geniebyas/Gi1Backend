ee7c8428fbaa170c824338915038f97b7ec254e3
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-15 00:09:03 +0530

    tyu erhdfh

--- app/Http/Controllers/API/ConnectionsController.php
+++ app/Http/Controllers/API/ConnectionsController.php
@@ -40,7 +40,7 @@ class ConnectionsController extends Controller
         }
 
         // Check if a request already exists
-        if (!$user->hasSentFriendRequest($dest_uid) && !$user->hasPendingFriendRequest($dest_uid) && !$user->isFriendWith($dest_uid)) {
+        if (!$user->hasSentFriendRequest($dest_uid) && !$user->isFriendWith($dest_uid)) {
             UsersConnection::create([
                 'source_uid' => $user->uid,
                 'dest_uid' => $dest_uid,
@@ -108,6 +108,7 @@ class ConnectionsController extends Controller
             $user->is_in_connectors = UsersConnection::where("dest_uid",$source_uid)->exists();
             $user->connectors_count= $user->connectorsCount();
             $user->connections_count= $user->connectionsCount();
+            $user->is_pending_request = $user->hasSentFriendRequest($uid);
             return response()->json(
                 [
                     'message' => "User Found",
