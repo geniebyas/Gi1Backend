adf218c04b42c08f158fbbc93ee0b6e7b0038eb3
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-16 11:32:35 +0530

    sdfsdf

--- app/Http/Controllers/API/ConnectionsController.php
+++ app/Http/Controllers/API/ConnectionsController.php
@@ -9,33 +9,34 @@ use Illuminate\Http\Request;
 
 class ConnectionsController extends Controller
 {
-    function getUserConnections(Request $request){
+    function getUserConnections(Request $request)
+    {
         $uid = $request->header('uid');
-        $user = User::with('connections')->where('uid',$uid)->first();
+        $user = User::with('connections')->where('uid', $uid)->first();
 
         return response()->json(
             [
-                'message'=> 'User loaded',
+                'message' => 'User loaded',
                 'status' => 1,
-                'data' =>$user
+                'data' => $user
             ],
             200
         );
     }
 
-    public function sendFriendRequest(Request $request,$dest_uid)
+    public function sendFriendRequest(Request $request, $dest_uid)
     {
-        $user = User::where('uid',$request->header('uid'))->first();
-        $dest_user = User::where('uid',$dest_uid)
-        ->with('settings')
-        ->get()
-        ->first();
-   
+        $user = User::where('uid', $request->header('uid'))->first();
+        $dest_user = User::where('uid', $dest_uid)
+            ->with('settings')
+            ->get()
+            ->first();
+
         $status = "pending";
         $setting = $dest_user->settings;
-        if($setting->is_private){
+        if ($setting->is_private) {
             $status = "pending";
-        }else{
+        } else {
             $status = "accepted";
         }
 
@@ -47,23 +48,26 @@ class ConnectionsController extends Controller
                 'status' => $status,
             ]);
 
-            return response()->json(['message' => 'Friend request sent.',
-            'status' =>'1',
-            'data' => "success"
-        ], 200);
+            return response()->json([
+                'message' => 'Friend request sent.',
+                'status' => '1',
+                'data' => "success"
+            ], 200);
         }
 
-        return response()->json(['message' => 'Unable to send friend request.',
-        'status' => 0,
-        'data' => "unable to send friend request"
-    ], 400);
+        return response()->json([
+            'message' => 'Unable to send friend request.',
+            'status' => 0,
+            'data' => "unable to send friend request"
+        ], 400);
     }
 
 
-    public function deleteRequest(Request $request,$dest_uid){
+    public function deleteRequest(Request $request, $dest_uid)
+    {
         $source_uid = $request->header('uid');
-        $connection = UsersConnection::where('source_uid',$source_uid)->where("dest_uid",$dest_uid)->get()->first();
-        if(is_null($connection)){
+        $connection = UsersConnection::where('source_uid', $source_uid)->where("dest_uid", $dest_uid)->get()->first();
+        if (is_null($connection)) {
             return response()->json(
                 [
                     'message' => 'You\'r not connected',
@@ -71,9 +75,8 @@ class ConnectionsController extends Controller
                     'data' => null
                 ],
                 400
-                );
-
-        }else{
+            );
+        } else {
             $connection->delete();
             return response()->json(
                 [
@@ -84,31 +87,31 @@ class ConnectionsController extends Controller
                 200
             );
         }
-
     }
 
-    function getUserWithDetails(Request $request,$uid){
+    function getUserWithDetails(Request $request, $uid)
+    {
         $source_uid = $request->header('uid');
-        if(User::find($uid)->exists()){
-            $user = User::where('uid',$uid)
-            ->with("wallet")
-            ->with("settings")
-            ->with('responses.question.category')
-            ->with('transactions.action')
-            ->with(["connections.destUser" => function ($query) {
-                $query->withCount('connections', 'connectors');
-            }])
-            ->with(["connectors.sourceUser" => function ($query) {
-                $query->withCount('connections', 'connectors');
-            }])
-            
-            ->get()
-            ->first();
-            $user->is_in_connections = UsersConnection::where("source_uid",$source_uid)->where("dest_uid",$uid)->where('status','accepted')->exists();
-            $user->is_in_connectors = UsersConnection::where("dest_uid",$source_uid)->where("source_uid",$uid)->where('status','accepted')->exists();
-            $user->connectors_count= $user->connectorsCount();
-            $user->connections_count= $user->connectionsCount();
-            $user->is_pending_request = UsersConnection::where("source_uid",$source_uid)->where("dest_uid",$uid)->where('status','pending')->exists();
+        if (User::find($uid)->exists()) {
+            $user = User::where('uid', $uid)
+                ->with("wallet")
+                ->with("settings")
+                ->with('responses.question.category')
+                ->with('transactions.action')
+                ->with(["connections.destUser" => function ($query) {
+                    $query->withCount('connections', 'connectors');
+                }])
+                ->with(["connectors.sourceUser" => function ($query) {
+                    $query->withCount('connections', 'connectors');
+                }])
+
+                ->get()
+                ->first();
+            $user->is_in_connections = UsersConnection::where("source_uid", $source_uid)->where("dest_uid", $uid)->where('status', 'accepted')->exists();
+            $user->is_in_connectors = UsersConnection::where("dest_uid", $source_uid)->where("source_uid", $uid)->where('status', 'accepted')->exists();
+            $user->connectors_count = $user->connectorsCount();
+            $user->connections_count = $user->connectionsCount();
+            $user->is_pending_request = UsersConnection::where("source_uid", $source_uid)->where("dest_uid", $uid)->where('status', 'pending')->exists();
             return response()->json(
                 [
                     'message' => "User Found",
@@ -117,8 +120,7 @@ class ConnectionsController extends Controller
                 ],
                 200
             );
-
-        }else{
+        } else {
             return response()->json(
                 [
                     'message' => 'User not found',
@@ -126,21 +128,22 @@ class ConnectionsController extends Controller
                     'data' => "No Data Found"
                 ],
                 402
-                );
+            );
         }
     }
 
 
-    public function getPendingRequest(Request $request){
+    public function getPendingRequest(Request $request)
+    {
         $uid = $request->header('uid');
 
-        $requests = UsersConnection::where('dest_uid',$uid)
-        ->with(["sourceUser" => function ($query) {
-            $query->withCount('connections', 'connectors');
-        }])
-        ->get();
+        $requests = UsersConnection::where('dest_uid', $uid)
+            ->with(["sourceUser" => function ($query) {
+                $query->withCount('connections', 'connectors');
+            }])
+            ->get();
 
-        if($requests->count() > 0){
+        if ($requests->count() > 0) {
             return response()->json(
                 [
                     'message' => 'Pending Requests',
@@ -149,7 +152,7 @@ class ConnectionsController extends Controller
                 ],
                 200
             );
-        }else{
+        } else {
             return response()->json(
                 [
                     'message' => 'Pending Requests',
@@ -159,63 +162,69 @@ class ConnectionsController extends Controller
                 200
             );
         }
-
     }
 
-    public function updateRequest($id,$status){
+    public function updateRequest($id, $status)
+    {
         $connection = UsersConnection::find($id);
-        if($status == "accepted"){
+        if ($status == "accepted") {
             $connection->status = $status;
             $connection->update();
             return response()->json([
                 'message' => 'Request accepted successfully',
                 'status' => 1,
                 'data' => "Request accepted successfully"
-            ],200);
-        }else{
+            ], 200);
+        } else {
             $connection->delete();
             return response()->json([
                 'message' => 'Request rejected successfully',
                 'status' => 0,
                 'data' => "Request rejected successfully"
-            ],200);
+            ], 200);
         }
     }
 
     public function getUserRelation(Request $request, $dest_uid)
-{
-    $source_uid = $request->header('uid');
+    {
+        $source_uid = $request->header('uid');
 
-    $dest_user = User::where("uid", $dest_uid)
-        ->with('connectors')
-        ->with('connections')
-        ->first();
+        $dest_user = User::where("uid", $dest_uid)
+            ->with('connectors')
+            ->with('connections')
+            ->first();
 
-    $source_user = User::where("uid", $source_uid)
-        ->with('connectors')
-        ->with('connections')
-        ->first();
+        $source_user = User::where("uid", $source_uid)
+            ->with('connectors')
+            ->with('connections')
+            ->first();
 
+        $dest_user->mutuals = $this->getMutualConnections($source_uid, $dest_uid);
 
-        $connectorsDest = is_array($dest_user->connectors) ? $dest_user->connectors : [];
-        $connectorsSource = is_array($source_user->connectors) ? $source_user->connectors : [];
-        $connectionsDest = is_array($dest_user->connections) ? $dest_user->connections : [];
-        $connectionsSource = is_array($source_user->connections) ? $source_user->connections : [];
-        
-        // Find common elements
-        $connectorsMutual = array_values(array_intersect($connectorsDest, $connectorsSource));
-        $connectionsMutual = array_values(array_intersect($connectionsDest, $connectionsSource));
-        
-    // Add mutuals property to $dest_user
-   $dest_user->mutuals = array_intersect($connectorsMutual,$connectionsMutual);
+        return response()->json([
+            'message' => 'User Relations',
+            'status' => 1,
+            'data' => $dest_user
+        ]);
+    }
 
-   
-    return response()->json([
-        'message' => 'User Relations',
-        'status' => 1,
-        'data' => $dest_user
-    ]);
-}
+    public function getMutualConnections($sourceUid, $destUid)
+    {
+        // Retrieve connections for the source user
+        $sourceConnections = UsersConnection::where('source_uid', $sourceUid)
+            ->where('status', 'accepted')
+            ->pluck('dest_uid')
+            ->toArray();
 
+        // Retrieve connections for the destination user
+        $destConnections = UsersConnection::where('source_uid', $destUid)
+            ->where('status', 'accepted')
+            ->pluck('dest_uid')
+            ->toArray();
 
+        // Find the intersection of connections
+        $mutualConnections = array_intersect($sourceConnections, $destConnections);
+
+        return $mutualConnections;
+    }
 }
