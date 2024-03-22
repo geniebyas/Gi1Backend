18d285ccd47985598a26ee9158b98b24ce4ee5a0
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-23 01:25:41 +0530

    dsfasdfsdf

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -5,45 +5,106 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\RawMessageFromArray;
 
 class NotificationController extends Controller
 {
     //
-    public function sendNotification(){
-        $client = new Client();
-        $resp = $client->request('POST', "https://fcm.googleapis.com/v1/projects/gi1-info-app/messages:send", [
-            'headers' => [
-                'Authorization' =>'Bearer c9afc9a63f4b178598d17edfa08d92a9f1e723b6',
-                'Content-Type' => 'application/json'
+    public function sendNotification()
+    {
+
+        define("GOOGLE_APPLICATION_CREDENTIALS", __DIR__ . '/gi1-info-app-c9afc9a63f4b.json');
+        $factory = (new Factory)->withServiceAccount(GOOGLE_APPLICATION_CREDENTIALS);
+        $messaging = $factory->createMessaging();
+
+
+        $messagee = new RawMessageFromArray([
+            'notification' => [
+                // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#notification
+                'title' => 'Default title',
+                'body' => 'Default body',
             ],
-            'form_params' => [
-                "message" => [
-                    "topic" => "all",
-                    "notification" => [
-                        "title" => "Testing Message",
-                        "body" => "This is a Test notification from the server."
+            'data' => [
+                'key' => 'Value',
+            ],
+            'android' => [
+                // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#androidconfig
+                'notification' => [
+                    'title' => 'Android Title',
+                    'body' => 'Android Body',
+                ],
+            ],
+            'apns' => [
+                // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#apnsconfig
+                'payload' => [
+                    'aps' => [
+                        'alert' => [
+                            'title' => 'iOS Title',
+                            'body' => 'iOS Body',
+                        ],
                     ],
-                    "data" => [
-                        "uid" => "test"
-                    ],
-                    "android" => [
-                        "notification" => [
-                            "body" => "This is a Test notification from the server"
-                        ]
-                    ],
-                    "apns" => [
-                        "payload" => [
-                            "aps" => [
-                                "category" => "Gi1 Prime App"
-                            ]
-                        ]
-                    ]
-                    
-                ]
+                ],
+            ],
+            'webpush' => [
+                // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#webpushconfig
+                'notification' => [
+                    'title' => 'Webpush Title',
+                    'body' => 'Webpush Body'
+                ],
+            ],
+            'fcm_options' => [
+                // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#fcmoptions
+                'analytics_label' => 'some-analytics-label'
             ]
         ]);
 
-        return response()->json($resp);
+        // $message = CloudMessage::withTarget('token', "dGslG6MHRRqHhflqZc4KlV:APA91bGpZTtPboCQ_9BloO5TvmgEo7wqBfYh_7fyGnxnfDutH0hWqWH1JHHH25JJO1DImRil68mVjWeNc6wN75CGcZOA1SP6WWazHI8b3r_l_BQ9dt3M5mm_g7lYvX2atLykeKn14z8w")
+        //     ->withNotification(['title' => 'My title', 'body' => 'My Body']);
+
+        $message = CloudMessage::withTarget('topic', "all")
+            ->withNotification(['title' => 'My title', 'body' => 'My Body']);
+
+        $messaging->send($message);
+
+
+
+
+        // $client = new Client();
+        // $resp = $client->request('POST', "https://fcm.googleapis.com/v1/projects/gi1-info-app/messages:send", [
+        //     'headers' => [
+        //         'Authorization' =>'Bearer c9afc9a63f4b178598d17edfa08d92a9f1e723b6',
+        //         'Content-Type' => 'application/json'
+        //     ],
+        //     'form_params' => [
+        //         "message" => [
+        //             "topic" => "all",
+        //             "notification" => [
+        //                 "title" => "Testing Message",
+        //                 "body" => "This is a Test notification from the server."
+        //             ],
+        //             "data" => [
+        //                 "uid" => "test"
+        //             ],
+        //             "android" => [
+        //                 "notification" => [
+        //                     "body" => "This is a Test notification from the server"
+        //                 ]
+        //             ],
+        //             "apns" => [
+        //                 "payload" => [
+        //                     "aps" => [
+        //                         "category" => "Gi1 Prime App"
+        //                     ]
+        //                 ]
+        //             ]
+
+        //         ]
+        //     ]
+        // ]);
+
+        // return response()->json($resp);
 
     }
 }
