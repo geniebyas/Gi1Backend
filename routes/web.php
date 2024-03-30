1dde3bda7ff656a8aaff203bcb80bad0fc58b0f3
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-03-30 13:04:29 +0530

    asdfasdf

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('/login');
-});
-
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/send-notification', [NotificationController::class, 'index'])->middleware('web');
