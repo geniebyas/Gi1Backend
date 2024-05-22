976e54348a40e39cb8d76ea350d7bea06ec02145
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-22 22:56:23 +0530

    Changes

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -6,41 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Models\PersonalNotification;
 use App\Models\PublicNotification;
 use Illuminate\Http\Request;
+
 class NotificationController extends Controller
 {
-    public function getNotifications(Request $request){
+    public function getNotifications(Request $request)
+    {
         $uid = $request->header('uid');
 
-        $publicNotifications = PublicNotification::where("is_announcement",true)->get();
-        $personalNotifications = PersonalNotification::where("receiver_uid",$uid)->with('sender')->get();
+        $publicNotifications = PublicNotification::where("is_announcement", true)->get();
+        $personalNotifications = PersonalNotification::where("receiver_uid", $uid)->with('sender')->get();
         return response()->json([
             'message' => 'Notification Collected',
-            'status'=> 1,
-            'data'=> [
-                'public'=>$publicNotifications,
-                'personal'=>$personalNotifications
+            'status' => 1,
+            'data' => [
+                'public' => $publicNotifications,
+                'personal' => $personalNotifications
             ]
         ]);
     }
 
-    public function getAnnouncement(Request $request){
+    public function getAnnouncement(Request $request)
+    {
         $announcement = PublicNotification::where("is_announcement", true)
             ->orderBy('created_at', 'desc')
             ->first();
-            
-                    if($announcement != []){
+
+        if ($announcement != null) {
             return response()->json([
-                "message"=>"Announcement Loaded",
-                "status"=>1,
-                "data"=>$announcement[0]
+                "message" => "Announcement Loaded",
+                "status" => 1,
+                "data" => $announcement[0]
             ]);
-        }else{
+        } else {
             return response()->json([
-                "message"=>"No Announcement Found",
-                "status"=>0,
-                "data"=>null
+                "message" => "No Announcement Found",
+                "status" => 0,
+                "data" => null
             ]);
         }
     }
-
 }
